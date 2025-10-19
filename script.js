06e4cfe466d45a99aa53050ebbe117c7be713b16
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-10-19 19:13:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let obj={};
+	for(let i=0;i<str.length;i++){
+		const char = str[i];
+	    if (char in obj) {
+		    obj[char] += 1;
+	    } 
+		else {
+			obj[char] = 1;
+	    }
+	}
+	for (let i = 0; i < str.length; i++) {
+		if(obj[str[i]]==1){
+			return str[i];
+		}
+	}
+	return "null";
 }
+firstNonRepeatedChar("aabbcdd");
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
